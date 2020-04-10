3fa6b645b8f909bce1f147d45c52cb7645fe68e8
johnebri <john.ebri@yahoo.com>
2020-04-10 21:17:00 +0100

    corrections

--- src/MyEstimator.js
+++ src/MyEstimator.js
@@ -4,16 +4,16 @@ const covidEstimator = require('./estimator.js');
 module.exports = {
   async fetch(req, res) {
     try {
-      const responseData =  await covidEstimator(req.body);
+      const responseData = await covidEstimator(req.body);
       return res.status(200).json({
         data: responseData.data,
         impact: responseData.impact,
         severeImpact: responseData.severeImpact
       });
     } catch (error) {
-        return res.status(400).json({
-          message: 'An error has occurred'
-        });
+      return res.status(400).json({
+        message: 'An error has occurred'
+      });
     }
   },
   async fetchxml(req, res) {
@@ -30,4 +30,4 @@ module.exports = {
       });
     }
   }
-}
+};
