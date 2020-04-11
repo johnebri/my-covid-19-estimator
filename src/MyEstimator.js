6299243ef44a8d4995048009fd6a07ee96dc3cdd
johnebri <john.ebri@yahoo.com>
2020-04-11 18:40:09 +0100

    updates

--- src/MyEstimator.js
+++ src/MyEstimator.js
@@ -1,6 +1,5 @@
-// const covid19 = require('./estimator.js');
-const covidEstimator = require('./estimator.js');
 const xml = require('xml');
+const covidEstimator = require('./estimator.js');
 
 module.exports = {
   async fetch(req, res) {
@@ -20,15 +19,14 @@ module.exports = {
   async fetchxml(req, res) {
     try {
       const responseData = await covidEstimator(req.body);
-      const output = res.status(200).json({
+      return res.status(200).json({
         data: responseData.data,
         impact: responseData.impact,
         severeImpact: responseData.severeImpact
       });
       // res.set('Content-Type', 'text/xml');
-      res.type('application/xml');
-      res.send(xml(output));
-      
+      // .type('application/xml');
+      // res.send(xml(output));
     } catch (error) {
       return res.status(400).json({
         message: 'An error has occurred'
